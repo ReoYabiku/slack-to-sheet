29d305fbc4062fad88b5b250495e4233e8ee3f8d
ReoYabiku <alcremikyu.z@gmail.com>
2022-09-22 23:41:38 +0900

    challenge Auth

--- cmd/main.go
+++ cmd/main.go
@@ -8,13 +8,20 @@ import (
 	echo "github.com/labstack/echo/v4"
 )
 
+type request struct {
+	Challenge string 	`json:challenge`
+}
+
 func main() {
 	e := echo.New()
 	e.POST("/slack/challenge", func(c echo.Context) error {
-		challenge := c.FormValue("challenge")
-		fmt.Printf("query: %s", challenge)
+		req := request{}
+		if err := c.Bind(&req); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
 		log.Println("got an access!!")
-		return c.String(http.StatusOK, challenge)
+		log.Printf("challenge: %s\n", req.Challenge)
+		return c.JSON(http.StatusOK, req)
 	})
 	e.GET("/", func(c echo.Context) error {
 		log.Println("got an access!!")
