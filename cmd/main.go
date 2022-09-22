21f6463bb1e8fe33b5e61591478fb81e205f94fd
ReoYabiku <alcremikyu.z@gmail.com>
2022-09-22 06:14:45 +0000

    eternalize container

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	echo "github.com/labstack/echo/v4"
@@ -10,9 +11,14 @@ import (
 func main() {
 	e := echo.New()
 	e.POST("/slack/challenge", func(c echo.Context) error {
-		challenge := c.QueryParam("challenge")
+		challenge := c.FormValue("challenge")
 		fmt.Printf("query: %s", challenge)
+		log.Println("got an access!!")
 		return c.String(http.StatusOK, challenge)
+	})
+	e.GET("/", func(c echo.Context) error {
+		log.Println("got an access!!")
+		return c.String(http.StatusOK, "hoge")
 	})
 	e.Logger.Fatal(e.Start(":80"))
 }
