94ceb46a1249e9c7d4a3e7fc817f70aa44162bbd
ReoYabiku <alcremikyu.z@gmail.com>
2022-09-22 17:17:57 +0000

    make port public

--- cmd/main.go
+++ cmd/main.go
@@ -11,17 +11,23 @@ type request struct {
 	Challenge string `json:"challenge"`
 }
 
-func main() {
-	e := echo.New()
-	e.POST("/event", func(c echo.Context) error {
-		req := request{}
-		if err := c.Bind(&req); err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-		}
-		log.Println("got an access!!")
+func slackAuth(c echo.Context) error {
+	log.Println("got an access!!")
+	req := request{}
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if req.Challenge != "" {
 		log.Printf("challenge: %s\n", req.Challenge)
 		return c.JSON(http.StatusOK, req)
-	})
+	} else {
+		return c.JSON(http.StatusOK, request{Challenge: "not challenge"})
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.POST("/event", slackAuth)
 	e.GET("/", func(c echo.Context) error {
 		log.Println("got an access!!")
 		return c.String(http.StatusOK, "hoge")
